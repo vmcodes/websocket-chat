907d0e3a66a47f0598acf05fb189e6cf360794ca
vincent <vmcodes05@gmail.com>
2022-05-13 16:07:22 -0400

    first commit

--- server/app/routes/message.routes.js
+++ server/app/routes/message.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const messages = require("../controllers/message.controller");
   const router = require("express").Router();
 
-  // Create a new Tutorial
+  // Create a new message
   router.post("/messages", messages.create);
 
   // Retrieve all messages
